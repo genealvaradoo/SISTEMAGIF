8abdf10c5e1dc6ce8d04355e30405acb58d99db0
genealvaradoo <genesisescarlethernandezalvarado@inued.net>
2024-05-29 07:24:10 -0600

    Add files via upload

---
+++ sistemas/sistem/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace sistem
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ sistemas/sistemas/frmGrafica.vb
@@ -0,0 +1,23 @@
+﻿Imports System.Data.OleDb
+Public Class frmGrafica
+    Private Sub frmGrafica_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim con As New OleDbConnection("Provider=Microsoft.Jet.OleDb.4.0; Data Source=E:\aja sistemas\MatriculaINUED(2002-2003)\MatriculaINUED(2002-2003).mdb")
+        Try
+            con.Open()
+            MsgBox("Conectado")
+
+            Dim query = "Select * from maestro"
+            Dim adap As New OleDbDataAdapter(query, con)
+            Dim dt As New DataTable
+            adap.Fill(dt)
+
+            Chart1.Series("Series1").XValueMember = "nombremaestro"
+            Chart1.Series("Series1").YValueMembers = "edadmaestro"
+            Chart1.DataSource = dt
+
+        Catch ex As Exception
+            MsgBox("No se conecto por: " & ex.Message)
+
+        End Try
+    End Sub
+End Class
